2425193ee163d4d990687bde0876d6cda6cc1c90
ankitjnvynr <ankitbkana@outlook.com>
2024-11-20 02:46:01 -0800

    update menchester page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,6 @@
-// Login.js
-
 "use client";
 
 import React, { useState, useEffect } from "react";
-import Link from "next/link";
-import { FaGithub, FaGoogle, FaApple } from "react-icons/fa";
 import { useRouter } from "next/navigation";
 import { toast } from "react-toastify";
 
@@ -14,8 +10,6 @@ const Login = () => {
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
-  const [validDomainStr, setValidDomainStr] = useState("");
-  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
 
   const allowedDomains = [
     "gmail.com",
@@ -26,7 +20,6 @@ const Login = () => {
   ];
 
   useEffect(() => {
-    setValidDomainStr(allowedDomains.join(", "));
     const token = localStorage.getItem("authToken");
     if (token) {
       router.push("/dashboard");
@@ -34,16 +27,6 @@ const Login = () => {
       setLoading(false);
     }
   }, [router]);
-
-  useEffect(() => {
-    const handleMouseMove = (e) => {
-      setMousePosition({ x: e.clientX, y: e.clientY });
-    };
-    window.addEventListener("mousemove", handleMouseMove);
-    return () => {
-      window.removeEventListener("mousemove", handleMouseMove);
-    };
-  }, []);
 
   const checkDomain = (e) => {
     if (!e) {
@@ -53,7 +36,7 @@ const Login = () => {
     }
     const emailDomain = e.split("@")[1];
     if (!allowedDomains.includes(emailDomain)) {
-      setError("Invalid Domain name");
+      setError("Invalid Domain Name");
       setEmail(e);
     } else {
       setError("");
@@ -93,83 +76,99 @@ const Login = () => {
   if (loading) return null;
 
   return (
-    <div
-      className="flex justify-center items-center min-h-screen bg-black text-white"
-      style={{
-        backgroundImage: `radial-gradient(circle at ${mousePosition.x}px ${mousePosition.y}px, rgba(70, 130, 180, 0.2), rgba(0, 0, 0, 1))`,
-      }}
-    >
-      <div className="flex mx-2 flex-col items-center text-center p-8 max-w-sm w-full bg-opacity-20 bg-black backdrop-blur-md shadow-lg rounded-lg border border-gray-600 shadow-gray-800">
-        {/* <img className="w-10 mb-4" src="/unicode.png" alt="logo" /> */}
-
-        <h3 className="text-4xl mb-10 font-bold my-4">Sign In</h3>
-
-        <div className="space-y-3 w-full">
-          {/* <button className="flex items-center justify-center w-full py-2 border border-gray-500 rounded-md hover:bg-gray-800">
-            <FaGithub className="mr-2" /> Sign in with Github
-          </button> */}
-          {/* <button className="flex items-center justify-center w-full py-2 border border-gray-500 rounded-md hover:bg-gray-800">
-            <FaGoogle className="mr-2" /> Sign in with Google
-          </button> */}
-          {/* <button className="flex items-center justify-center w-full py-2 border border-gray-500 rounded-md hover:bg-gray-800">
-            <FaApple className="mr-2" /> Sign in with Apple
-          </button> */}
-        </div>
-
-        {/* <div className="flex items-center my-6 w-full">
-          <hr className="flex-grow border-gray-600" />
-          <span className="px-2 text-gray-400">OR</span>
-          <hr className="flex-grow border-gray-600" />
-        </div> */}
-
-        <form
-          onSubmit={handleLogin}
-          className="w-full space-y-5 flex flex-col "
-        >
-          <input
-            type="email"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => checkDomain(e.target.value)}
-            className="w-full px-4 py-2 bg-gray-800 text-gray-200 rounded-md outline-none focus:ring-2 focus:ring--500"
-            required
-          />
-          {error && (
-            <p style={{
-              marginTop:'-1px'
-            }} className="text-yellow-500 text-xs text-left -mt-2">{error}</p>
-          )}
-          {/* <span className="text-xs text-left text-gray-400">{validDomainStr}</span> */}
-
-          <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="w-full px-4 py-2 bg-gray-800 text-gray-200 rounded-md outline-none focus:ring-2 focus:ring--500"
-            required
-          />
-          <div className="w-full text-right">
-            <a href="#" className="text-xs text-gray-400 hover:underline">
-              Forgot your password?
-            </a>
+    <div className="flex flex-col justify-between min-h-screen bg-black text-white">
+      {/* Header */}
+      <header className="flex justify-between items-center px-6 py-4 text-sm border-b border-gray-800">
+        <div className="text-lg font-bold">UNICORD®</div>
+        <div className="flex items-center space-x-6">
+          {/* Search Bar */}
+          <div className="relative shadow-lg">
+          <span className="absolute left-2 top-2 text-gray-500">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-4 w-4"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M21 21l-4.35-4.35m1.9-5.4a7.5 7.5 0 11-15 0 7.5 7.5 0 0115 0z"
+                />
+              </svg>
+            </span>
+            <input
+              type="text"
+              placeholder="type anything"
+              className="px-0 pl-16 py-2 bg-transparent border-b border-white text-white italic outline-none placeholder-gray-0 focus:ring-0 focus:ring-gray-0"
+            />
+          
           </div>
+          <a href="#" className="text-gray-400 hover:underline">
+            For Enterprise
+          </a>
+        </div>
+      </header>
 
-          <button
-            type="submit"
-            className="w-full py-2 bg-white text-black rounded-md font-semibold transition duration-300 ease-in-out hover:bg-gray-200"
+      {/* Main Content */}
+      <main className="flex flex-col items-center justify-center flex-grow px-4">
+        {/* Title */}
+        <h1 className="text-3xl text-center mb-8 font-light border-b w-full pb-9 border-gray-800">
+          “The safest <span className="font-bold italic">social network</span>{" "}
+          in the universe”
+        </h1>
+        
+
+        {/* Login Card */}
+        <div className="p-6 py-11 bg-opacity-10 bg-gray-400 backdrop-blur-md rounded-3xl shadow-2xl  border-gray-600 w-full max-w-sm">
+          <h3 className="text-xl font-bold mb-6 text-center">UNICORD®</h3>
+
+          <form
+            onSubmit={handleLogin}
+            className="w-full space-y-4  text-center"
           >
-            Sign in
-          </button>
-        </form>
+            <input
+              type="email"
+              placeholder="Username"
+              value={email}
+              onChange={(e) => checkDomain(e.target.value)}
+              className="w-full px-4 py-2 bg-opacity-10 bg-gray-400 text-white rounded-full outline-none   focus:ring-0 focus:ring-gray-500 shadow-md"
+              required
+            />
+            {error && (
+              <p className="text-yellow-500 text-xs mt-1">{error}</p>
+            )}
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-full px-4 py-2 bg-opacity-10 bg-gray-400 text-white rounded-full outline-none   focus:ring-0 focus:ring-gray-500 shadow-md"
+              required
+            />
+            <div className="text-right">
+              <a href="#" className="text-xs text-gray-400 hover:underline">
+                forgot password?
+              </a>
+            </div>
 
-        <p className="mt-4 text-sm text-gray-400">
-          Don’t have an account?{" "}
-          <Link href="/login/register" className="text-white hover:underline">
-            Sign up
-          </Link>
-        </p>
-      </div>
+            {/* Login Button */}
+            <button
+              type="submit"
+              className="w-1/2 m-auto  px-4 py-2 bg-opacity-10 bg-gray-400 text-white rounded-full outline-none   focus:ring-0 focus:ring-gray-500 shadow-md"
+            >
+              Login
+            </button>
+          </form>
+        </div>
+      </main>
+
+      {/* Footer */}
+      <footer className="text-center text-xs text-gray-500 py-4 border-t border-gray-800">
+        All Rights Reserved ©UNICORD® 2024
+      </footer>
     </div>
   );
 };
